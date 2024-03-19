6623c09406f48fa7b48e172dcc0a05553c95963d
arthur <arthur.queiroz08@aluno.ifce.edu.br>
2024-03-19 18:01:32 -0300

    Initial commit

---
+++ src/main/java/com/java/nuven/NuvenApplication.java
@@ -0,0 +1,13 @@
+package com.java.nuven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuvenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuvenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/nuven/NuvenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.nuven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuvenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
